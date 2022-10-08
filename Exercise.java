f08b1a381063346af90ed09d6dc8ff208a03174d
tlaze <tomlazore@gmail.com>
2022-10-08 19:19:26 -0400

    Added constructor variables and methods to Exercise class

--- Exercise.java
+++ Exercise.java
@@ -1,3 +1,33 @@
 public class Exercise {
+
+    private String exercise;
+    private int length;
+
+    public Exercise(String exercise, int length){
+        this.exercise = exercise;
+        this.length = length;
+    }
+
+    //GETTER and Setters
+    public String getExercise(){
+        return exercise;
+    }
+
+    public void setExercise(String exercise){
+        this.exercise = exercise;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public void setLength(int length){
+        this.length = length;
+    }
+
+    @Override
+    public String toString(){
+        return "You chose to do: " + this.exercise + " for a duration of " + this.length + " minutes.";
+    }
     
 }
